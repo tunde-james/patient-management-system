7a22e3407ca58eac14915a46b782bf5f8a49d68a
Tunde James <59791126+tunde-james@users.noreply.github.com>
2026-06-20 14:18:34 +0100

    build(patient-service): update dependencies and application config
    Adds PostgreSQL and Lombok dependencies to pom.xml, switches from application.properties to application.yml, and updates main application class for JPA support.

--- patient-service/src/main/java/com/devtunde/patientservice/PatientServiceApplication.java
+++ patient-service/src/main/java/com/devtunde/patientservice/PatientServiceApplication.java
@@ -2,12 +2,13 @@ package com.devtunde.patientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PatientServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PatientServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PatientServiceApplication.class, args);
+    }
 }
